2bda70dcbcf16b9529d299064e5b9ca1490fc6e2
root <root@169.254.143.154>
2026-08-19 09:57:20 +0000

    add(api): add robust Calendar task publisher

---
+++ apps/api/plane/integrations/google_calendar/dispatch.py
@@ -0,0 +1,14 @@
+# Copyright (c) 2023-present Plane Software, Inc. and contributors
+# SPDX-License-Identifier: AGPL-3.0-only
+# See the LICENSE file for details.
+
+from django.db import transaction
+
+
+def enqueue_google_calendar_task_on_commit(task, *args, **kwargs):
+    """Publish a Calendar task after the current database transaction commits."""
+
+    def _enqueue_google_calendar_task():
+        task.delay(*args, **kwargs)
+
+    transaction.on_commit(_enqueue_google_calendar_task, robust=True)
